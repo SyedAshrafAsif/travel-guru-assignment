66be13b968e15aa27142b6ef3d9c0d5ed9220802
noobcoderasif <syedashrafasif@gmail.com>
2020-09-17 17:57:18 +0600

    fixed some problems with card

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,60 +2,53 @@ import React, { useState } from 'react';
 import Header from '../Header/Header';
 import './Home.css';
 import {Card} from 'react-bootstrap';
-import sajekImg from '../../image/Sajek.png';
-import sreemongolImg from '../../image/Sreemongol.png';
-import sundorbonImg from '../../image/sundorbon.png';
 import cardInfo from '../../fakeData/cardInfo';
 
 const Home = () => {
     const data = cardInfo;
     const [destination, setDestination] = useState(data);
-    const [info, setCardInfo] = useState([]);
+    const [cardDetails, setCardDetails] = useState([]);
 
     const handleCardInfo = (cardInfo) => {
-        const newInfo = [cardInfo];
-        setCardInfo(newInfo);
+        const showDescription = document.getElementById('card-desc');
+        if (showDescription.style.display === "none") {
+            showDescription.style.display = "block";
+          } else {
+            showDescription.style.display = "none";
+          }
+        const newInfo = [cardDetails, cardInfo];
+        setCardDetails(newInfo);
         }
+
+        
 
     return (
         <div className="background">
             <Header></Header>
             <div className="adjust align-middle d-flex justify-content-center">
-                <div className="description nav justify-content-center text-white">
+                <div id="card-desc" className="description text-white">
                     {
-                        destination.map(cardInfo => {
-                            return <div>
-                                <h2>{cardInfo.title}</h2>
-                                <p>{cardInfo.description}</p>
-                            </div>                            
-                        })
-                    }
+                        destination.map(cardDesc => 
+                            <div>
+                                <h2>{cardDesc.title}</h2>
+                                <p>{cardDesc.description}</p>
+                            </div>                        
+                             )                           
+                    }                   
                 </div>
                 <div className="card-area">
-                    <div>
-                        <Card style={{ width: '12rem', margin: '5px'}} className="bg-transparent text-white">
-                            <Card.Img src={sajekImg} alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Title><h4>Sajek</h4></Card.Title>
-                            </Card.ImgOverlay>
-                        </Card>
-                    </div>
-                    <div>
-                        <Card style={{width: '12rem', margin: '5px'}} className="bg-transparent text-white">
-                            <Card.Img src={sreemongolImg} alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Title><h4>Sreemongol</h4></Card.Title>
-                            </Card.ImgOverlay>
-                        </Card>
-                    </div>
-                    <div>
-                        <Card style={{width: '12rem', margin: '5px'}} className="bg-transparent text-white">
-                            <Card.Img src={sundorbonImg} alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Title><h4>Sundarban</h4></Card.Title>
-                            </Card.ImgOverlay>
-                        </Card>
-                    </div>
+                    {
+                        destination.map((cardInfo) =>                                           
+                            <Card className="bg-transparent text-white card-display">
+                                <Card.Img src={cardInfo.img} alt="Card image" title="Double click on it!"/>
+                                <Card.ImgOverlay>
+                                    <Card.Title><h4 style={{textAlign: 'center'}}>{cardInfo.title}</h4></Card.Title>                                    
+                                    <a onClick={() => handleCardInfo(cardInfo)} href="#" className="stretched-link"></a>
+                                </Card.ImgOverlay>
+                            </Card>                        
+                        )    
+                    }                    
+                    
                 </div>                                                           
             </div>
         </div>
